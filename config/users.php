d55caf8037e7defbb487f5007be69aa7100f2382
MR.MIDO <126908280+MRMIDOO77@users.noreply.github.com>
2023-08-15 03:35:17 +0300

    Add files via upload

---
+++ config/users.php
@@ -0,0 +1,20 @@
+<?php
+
+$auchats = array('-1001848070236');
+$owner = '5340950052';
+$preusers = array();
+
+if($userId == '1764351005'){
+    $user = "<a href='tg://user?id=1764351005'>BADBOY</a>";
+  $plan = 'God';
+}
+elseif(in_array($userId,$preusers)){
+    $user = '@'.$username.'[𝗣𝗿𝗲𝗺𝗶𝘂𝗺✨]';
+    $plan = 'premium';
+}
+else {
+    $user = '@'.$username.'[𝗙𝗿𝗲𝗲]';
+    $plan = 'free';
+}
+
+?>
